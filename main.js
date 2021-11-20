b1a451fa95c6e2e9039b0170f662297439e8806a
sajidalam321 <alamraza812@gmail.com>
2021-11-20 22:58:39 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,32 @@
+const items = document.querySelectorAll
+('#timeline li');
+
+const isInViewport = el => {
+    const rect = el.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <=
+        (window.innerHeight ||
+        document.documentElement.clientHeight) &&
+        rect.right <=(window.innerWidth ||
+        document.documentElement.clientWidth)
+    );
+};
+
+const run = () =>
+items.forEach(item => {
+    if (isInViewport(item)) {
+        item.classList.add('show') ;
+    }
+});
+
+// Events
+Window.addEventlistener ('load',run);
+Window.addEventlistener('resize', run);
+Window.addEventlistener('scroll', run);
+
+// 
+
+
+
